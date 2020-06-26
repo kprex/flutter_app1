689f088011fd25e4882798cbb5ec7519323a22e0
unknown <you@example.com>
2020-06-26 10:33:34 +0800

    init flutter app file

---
+++ .dart_tool/flutter_build/5201b2e30d7d9c3cee784425231f32f9/x86_64/gen_snapshot.d
@@ -0,0 +1,1 @@
+gen_snapshot.d: D:\Program_Files2\flutter\bin\cache\artifacts\engine\android-x64-release\windows-x64\gen_snapshot
---
+++ lib/dart1.dart
@@ -0,0 +1,10 @@
+//main(){
+//  String h1="what time noe"
+//      "this time is"
+//      " 0200202002"
+//      ;
+//  print(h1);
+//  const s='zhangsan';
+//  print(s);
+//
+//}
