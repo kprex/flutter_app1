d64fde1049a1d5b4a5981dd8b3eaa152550b9758
yindl <ygzzjzzgy@163.com>
2022-04-19 15:27:58 +0800

    init yh

--- .dart_tool/flutter_build/5201b2e30d7d9c3cee784425231f32f9/armeabi-v7a/gen_snapshot.d
+++
@@ -1,1 +0,0 @@
-gen_snapshot.d: D:\Program_Files2\flutter\bin\cache\artifacts\engine\android-arm-release\windows-x64\gen_snapshot
---
+++ yh/android/app/src/main/kotlin/com/example/yh/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
